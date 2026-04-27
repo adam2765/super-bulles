18fba689f317fa76e3b1854b8b76555ef314edbb
adam2765 <76868200+adam2765@users.noreply.github.com>
2026-04-27 20:55:38 +0200

    Update affichage.c

--- affichage.c
+++ affichage.c
@@ -1,1 +1,8 @@
-
+/*
+ * affichage.c
+ * Rendu graphique avec Allegro 4.
+ * - Afficher une frame du jeu (joueur, bulles, projectile, HUD)
+ * - Afficher le menu principal avec option en surbrillance
+ * - Afficher l'écran de victoire et de défaite
+ * - Afficher le score, le temps restant et le niveau
+ */
