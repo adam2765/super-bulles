7f87e6f09a671d6b4cf8668a5caadfe6bea12dcf
adam2765 <76868200+adam2765@users.noreply.github.com>
2026-04-27 20:57:10 +0200

    Update sauvegarde.h

--- sauvegarde.h
+++ sauvegarde.h
@@ -1,1 +1,8 @@
-
+/*
+ * sauvegarde.h
+ * Header de la sauvegarde et du chargement.
+ * - Contient le prototype de sauvegarder()
+ * - Contient le prototype de chargerSauvegarde()
+ * - Les données sauvegardées : pseudo + numéro de niveau
+ * - Inclure ce fichier dans jeu.c et main.c
+ */
