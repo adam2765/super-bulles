e37ea3745ac92a3d6b7145e44c70841cfba96540
adam2765 <76868200+adam2765@users.noreply.github.com>
2026-04-27 20:52:31 +0200

    Update jeu.c

--- jeu.c
+++ jeu.c
@@ -1,1 +1,8 @@
-
+/*
+ * jeu.c
+ * Logique globale du jeu.
+ * - Initialiser une nouvelle partie (score, niveau, joueur)
+ * - Lancer un niveau et retourner victoire ou défaite
+ * - Contient la boucle principale du jeu (boucleJeu)
+ * - Gère l'enchainement des niveaux et les écrans fin de niveau
+ */
