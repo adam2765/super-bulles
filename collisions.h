725d4599cefacfef96d5032b48152cd9c2b3b7cb
adam2765 <76868200+adam2765@users.noreply.github.com>
2026-04-27 20:54:08 +0200

    Update collisions.h

--- collisions.h
+++ collisions.h
@@ -1,1 +1,8 @@
-
+/*
+ * collision.h
+ * Header de la détection des collisions.
+ * - Contient le prototype de collisionCercles()
+ * - Contient le prototype de collisionRectangles()
+ * - Contient le prototype de gererCollisions()
+ * - Inclure ce fichier dans jeu.c
+ */
