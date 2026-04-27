2349f6de8bf8cd0f994011edee21796a3046f55e
adam2765 <76868200+adam2765@users.noreply.github.com>
2026-04-27 20:55:07 +0200

    Update affichage.h

--- affichage.h
+++ affichage.h
@@ -1,1 +1,9 @@
-
+/*
+ * affichage.h
+ * Header du rendu graphique (Allegro 4).
+ * - Contient le prototype de afficherEcranJeu()
+ * - Contient le prototype de afficherMenu()
+ * - Contient le prototype de afficherVictoire()
+ * - Contient le prototype de afficherDefaite()
+ * - Inclure ce fichier dans jeu.c et main.c
+ */
