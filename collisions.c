2a3da17216508980ba130dc5ba25705192c9ba32
adam2765 <76868200+adam2765@users.noreply.github.com>
2026-04-27 20:54:36 +0200

    Update collisions.c

--- collisions.c
+++ collisions.c
@@ -1,1 +1,8 @@
-
+/*
+ * collision.c
+ * Détection et gestion des collisions.
+ * - Tester la collision entre deux cercles (tir/bulle)
+ * - Tester la collision entre un cercle et un rectangle (bulle/joueur)
+ * - Gérer toutes les collisions de la boucle de jeu
+ * - Appeler diviserBulle() si collision tir/bulle détectée
+ */
