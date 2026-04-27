46da59fd35fb21337fc285cb16620bb587a97892
adam2765 <76868200+adam2765@users.noreply.github.com>
2026-04-27 20:51:59 +0200

    Add header comments to jeu.h
    Added header comments to describe the game logic functions.

--- jeu.h
+++ jeu.h
@@ -1,1 +1,8 @@
-
+/*
+ * jeu.h
+ * Header de la logique globale du jeu.
+ * - Contient le prototype de initialiserPartie()
+ * - Contient le prototype de lancerNiveau()
+ * - Contient le prototype de boucleJeu()
+ * - Inclure ce fichier dans main.c et jeu.c
+ */
