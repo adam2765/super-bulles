441f512db87941a8594924e6ca6b45759f545a6c
adam2765 <76868200+adam2765@users.noreply.github.com>
2026-04-27 20:56:11 +0200

    Update input.h

--- input.h
+++ input.h
@@ -1,1 +1,9 @@
-
+/*
+ * input.h
+ * Header de la gestion des entrées clavier.
+ * - Contient le prototype de lireEntrees()
+ * - Gère les déplacements du joueur (flèches gauche/droite)
+ * - Gère le tir (espace)
+ * - Gère la navigation dans les menus
+ * - Inclure ce fichier dans jeu.c
+ */
