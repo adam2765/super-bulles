53d9a280719f216bdb2e198125d140a629a13834
adam2765 <76868200+adam2765@users.noreply.github.com>
2026-04-27 20:51:03 +0200

    Add header comment for main.c
    Added a header comment to describe the main program functionality.

--- main.c
+++ main.c
@@ -1,1 +1,8 @@
-
+/*
+ * main.c
+ * Point d'entrée du programme.
+ * - Initialise Allegro et la fenêtre
+ * - Affiche le menu principal
+ * - Lance la partie selon le choix utilisateur
+ * - Libère les ressources à la fin
+ */
