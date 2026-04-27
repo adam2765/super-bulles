32db3347c49922347de8c9d987ac0f79647f2b78
adam2765 <76868200+adam2765@users.noreply.github.com>
2026-04-27 20:57:44 +0200

    Add header comments to sauvegarde.c
    Added comments to describe the functionality of the file.

--- sauvegarde.c
+++ sauvegarde.c
@@ -1,1 +1,8 @@
-
+/*
+ * sauvegarde.c
+ * Sauvegarde et chargement des parties.
+ * - Écrire le pseudo et le numéro de niveau dans sauvegarde.txt
+ * - Lire la sauvegarde associée à un pseudo
+ * - Gérer plusieurs sauvegardes dans un même fichier
+ * - Retourner -1 si aucune sauvegarde trouvée pour ce pseudo
+ */
