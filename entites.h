d94c8c1289d2810ea62b53c5538e2f24c3f5391d
adam2765 <76868200+adam2765@users.noreply.github.com>
2026-04-27 20:53:09 +0200

    Add header documentation for entites.h
    Add header comments for game entities.

--- entites.h
+++ entites.h
@@ -1,1 +1,8 @@
-
+/*
+ * entites.h
+ * Header des entités du jeu.
+ * - Contient les structs : Joueur, Bulle, Projectile
+ * - Contient les prototypes de toutes les fonctions
+ *   liées au joueur, aux bulles et aux projectiles
+ * - Inclure ce fichier dans tous les autres modules
+ */
