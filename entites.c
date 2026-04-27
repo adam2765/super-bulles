1aa97d16bbf5fe5d705c616803bc2705a5f509a2
adam2765 <76868200+adam2765@users.noreply.github.com>
2026-04-27 20:53:39 +0200

    Update entites.c

--- entites.c
+++ entites.c
@@ -1,1 +1,8 @@
-
+/*
+ * entites.c
+ * Gestion des entités du jeu (joueur, bulles, projectiles).
+ * - Initialiser et déplacer le joueur
+ * - Ajouter, mettre à jour et diviser les bulles
+ * - Gérer les tirs (créer et déplacer les projectiles)
+ * - Gérer la sauvegarde et le chargement des parties
+ */
